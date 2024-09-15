c0593886e461a375603a3302c477e72cce2b2c7e
HatimAlgethami <Hh11034@>
2024-09-15 19:00:24 +0300

    History and Details pages

---
+++ app/src/main/java/com/example/myapplication/DetailsActivity.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+    }
+}
